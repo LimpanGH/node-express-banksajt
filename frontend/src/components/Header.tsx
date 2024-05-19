25f4e5f920999bdc63170896d467f512ea395327
LimpanGH <Linusj555@gmail.com>
2024-05-19 11:14:54 +0200

    committing all files

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 function Header() {
   return (
-    <header className="px-5 py-5 bg-gray-700 text-white text-center flex justify-between">
+    <header className="flex justify-between px-5 py-5 text-center text-white bg-gray-700">
       <h1 className="text-3xl font-bold"><Link to="/">Banksajt</Link></h1>
       <Navigation />
     </header>
@@ -12,3 +12,30 @@ function Header() {
 }
 
 export default Header;
+
+// import Navigation from "./Navigation";
+// import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+
+// function Header() {
+//   const [username, setUsername] = useState('');
+
+//   useEffect(() => {
+//     const storedUsername = localStorage.getItem('username');
+//     if (storedUsername) {
+//       setUsername(storedUsername);
+//     }
+//   }, []);
+
+//   return (
+//     <header className="flex justify-between px-5 py-5 text-center text-white bg-gray-700">
+//       <h1 className="text-3xl font-bold"><Link to="/">Banksajt</Link></h1>
+//       <div>
+//         <p>{username ? `Inloggad som: ${username}` : 'Inte inloggad'}</p>
+//         <Navigation />
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
